a5cc9bf8e3bcbf07db1c0dad82a6d13f0dddf4f2
nuannim <66070286@kmitl.ac.th>
2024-10-04 19:22:06 +0700

    Update ProfOperationController.py

--- ProfOperationController.py
+++ ProfOperationController.py
@@ -14,12 +14,15 @@ class ProfOperationController:
         return
     
     def getInfoFromDatabase(self): # get course from db
-
+        
 
     def popUp(self):
 
     def calculateTA(self):
 
     def delete(self):
+
+    def setFromCourseAddEdit(self, cName, cID, desc, image, req, ):
+
 
 
