99109ded6d4b09321cd13dc740f8cd28d35f99bb
nuannim <66070286@kmitl.ac.th>
2024-10-05 14:57:09 +0700

    update : run.py for getInfoFromModel()

--- run.py
+++ run.py
@@ -14,8 +14,31 @@ def main():
     # controller = ProfOperationController(pCourse, pCourseAddEdit, allCourse)
     # controller.sendToDatabase(allCourse, pCourse)
 
-    pass
-    
+    isadView = ProfCourseView()
+    isadAddEditView = ProfCourseAddnEditView()
+    isadCourse = CourseModel()
 
+    controller = ProfOperationController(isadView, isadAddEditView, isadCourse)
+
+    controller.sendToDatabase(
+                                isadCourse,
+                                cName="ISAD Course",
+                                cID=666666,
+                                desc="this is about isad ",
+                                image=None,
+                                req=None,
+                                reqToFill=0,
+                                adate=16-9-24,
+                                wdate=19-9-24,
+                                cdate=20-9-24,
+                                qtype=0,
+                                contact="0800000000"
+                              )
+
+    print('isad get name = ' + isadCourse.getName())
+
+    controller.getInfoFromModel(isadCourse, isadView)
+
+    pass
 
 main()
