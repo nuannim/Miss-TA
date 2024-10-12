a46868b3e9ebffdfaea68b6270ab9d7feb213524
nuannim <66070286@kmitl.ac.th>
2024-10-12 22:57:00 +0700

    database : update sql script

--- sql12737141 script.sql
+++ sql12737141 script.sql
@@ -154,3 +154,37 @@ create table sql12737141.student_req (
         Foreign Key (student_id) REFERENCES student(student_id)
             on delete CASCADE
 );
+
+alter table grade
+modify grade INT;
+
+delete from grade;
+
+DESCRIBE sql12737141.grade;
+select * from grade;
+
+
+alter table enroll
+add Foreign Key (course_id) REFERENCES course(course_id);
+
+alter table sql12737141.student
+add num_ta int;
+
+
+create table sql12737141.prof_course (
+    prof_course_id int primary key AUTO_INCREMENT,
+    course_id INT not NULL,
+    prof_id int not null,
+        Foreign Key (course_id) REFERENCES course(course_id)
+            on delete CASCADE,
+        Foreign Key (prof_id) REFERENCES professor(prof_id)
+            on delete CASCADE
+);
+
+describe sql12737141.course;
+describe sql12737141.professor;
+
+
+alter Table sql12737141.professor
+add PRIMARY key (prof_id);
+
--- sql12737141 script2.sql
+++ sql12737141 script2.sql
@@ -1,26 +1,3 @@
 use sql12737141;
 
-alter table enroll
-add Foreign Key (course_id) REFERENCES course(course_id);
-
-alter table sql12737141.student
-add num_ta int;
-
-
--- create table sql12737141.prof_course (
---     prof_course_id int primary key AUTO_INCREMENT,
---     course_id INT not NULL,
---     prof_id int not null,
---         Foreign Key (course_id) REFERENCES course(course_id)
---             on delete CASCADE,
---         Foreign Key (prof_id) REFERENCES professor(prof_id)
---             on delete CASCADE
--- );
-
--- describe sql12737141.course;
--- describe sql12737141.professor;
-
-
--- alter Table sql12737141.professor
--- add PRIMARY key (prof_id);
 
