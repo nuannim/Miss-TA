76a8d952ae5a4b0f6c194f71f925d2beee39a7a2
nuannim <66070286@kmitl.ac.th>
2024-10-14 17:47:01 +0700

    clear file

--- test/dbcontrollertest2.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-import mysql.connector
-
-cnx = mysql.connector.connect(user='scott', password='password',
-                              host='sql12.freesqldatabase.com',
-                              database='employees')
-cnx.close()
--- test/test.py
+++
@@ -1,8 +0,0 @@
-def main():
-    car = [1, 2, 4]
-    print('car[0] = ' + str(car[0]))
-    car.append('hey')
-
-    print(car)
-
-main()
