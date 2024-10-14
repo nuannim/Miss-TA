6d8dd69b9a2b1bf7470c59d90923a80fb61b7387
nuannim <66070286@kmitl.ac.th>
2024-10-14 20:35:10 +0700

    test

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+app = FastAPI()
+
+template = Jinja2Templates(directory='CJ')
+
+@app.get('/', )
+def index(req: Request):
+    return template.TemplateResponse(
+        name="admin_subject.html",
+        context={"request": req}
+    )
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
