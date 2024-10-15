e6d21757d24f4f0a4d4eec28e377060020597780
nontawic01 <146535660+nontawic01@users.noreply.github.com>
2024-10-15 23:23:21 +0700

    update:  generalController

--- generalController.py
+++ generalController.py
@@ -13,3 +13,16 @@ class generalController:
         if num:
             return num[0]['course_count']  # ดึงค่า course_count จากผลลัพธ์
         return 0  # ถ้าไม่มีข้อมูล
+    
+    def checkstatus(self, sid,cid,cyear):
+        grade = self.db.getStudentgrade(sid,cid)
+        year = self.db.getYearStudent(sid)
+        ta = self.db.getNumTa(sid)
+
+        if int(grade) >= 3 and year >= cyear and ta != 2:
+            return True
+        else:
+            return False
+    def getnamecourse(self,id):
+        return self.db.getCourseName(id)
+    
