e555121a14c75ea8a7ea14ea77df0f0114268da7
nontawic01 <146535660+nontawic01@users.noreply.github.com>
2024-10-17 20:50:05 +0700

    Update StudentModel.py

--- noeysod/StudentModel.py
+++ noeysod/StudentModel.py
@@ -1,3 +1,4 @@
+from Grade import *
 class StudentModel:
     def __init__(self, 
                  student_id="", 
@@ -5,22 +6,26 @@ class StudentModel:
                  last_name="", 
                  year=0, 
                  announce_list_status=False, 
-                 waiting_list_status=False, 
-                 grade=None, 
-                 wage=0.0):
+                 waiting_list_status=False,  
+                 wage=0.0,
+                 num_ta = 0):
         self._student_id = student_id
         self._first_name = first_name
         self._last_name = last_name
         self._year = year
         self._announce_list_status = announce_list_status
         self._waiting_list_status = waiting_list_status
-        self._grade = grade
+        self._grade = [Grade]
         self._wage = wage
+        self._num_ta = num_ta
 
 #^ getter
 
     def getStudentID(self):
         return self._student_id
+    
+    def getnum_ta(self):
+        return self._num_ta
 
     def getFirstName(self):
         return self._first_name
@@ -37,7 +42,7 @@ class StudentModel:
     def getWaitingL_Status(self):
         return self._waiting_list_status
 
-    def getGrade(self):
+    def getallGrade(self):
         return self._grade
 
     def getWage(self):
@@ -57,14 +62,21 @@ class StudentModel:
     def setYear(self, year):
         self._year = year
 
+    def setnum_ta(self, num_ta):
+        self._num_ta = num_ta
+
     def setAnnounceL_Status(self, status):
         self._announce_list_status = status
 
     def setWaitingL_Status(self, status):
         self._waiting_list_status = status
 
-    def setGrade(self, grade):
-        self._grade = grade
+    def setallGrade(self, grade):
+        if isinstance(grade, Grade):
+            self._grade.append(grade)
+        else:
+            raise TypeError("ต้องเป็น object ของคลาส grade เท่านั้น")
 
     def setWage(self, wage):
         self._wage = wage
+
