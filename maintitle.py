be7dd00a018cd1212a360487a74c61d4fa944b80
nontawic01 <146535660+nontawic01@users.noreply.github.com>
2024-10-20 19:06:55 +0700

    60%

--- maintitle.py
+++ maintitle.py
@@ -34,8 +34,9 @@ async def homepage(request: Request):
             name = i.getName()
             descrip = i.getDescription()
             cid = i.getCourseID()
+            chi = i.getCourseHistoryID()
             status = scc.checkstatus(cid)
-            tup = {"name": name, "status": status, "description": descrip, "index_id": i, "course_id": cid}
+            tup = {"name": name, "status": status, "description": descrip, "index_id": i, "course_id": cid ,"coursehistoryid" : chi}
             if status:
                 course_data.append(tup)
             else:
@@ -46,33 +47,25 @@ async def homepage(request: Request):
 
 @app.get("/course/{course_id}", response_class=HTMLResponse)
 async def course_detail(request: Request,course_id: int):
-    # print("Courseforreq")
-    # print(course_id)
-    # print("Courseforreq")
-    # print(course_id)
-    # course_data = []
-    # req_data = []
-    # nonreq_data = []
-    # req_list = [Requirement]
-    # for i in course:
-    #     if isinstance(i, CourseModel): 
-    #         if i.getCourseID() == course_id:
-    #             name = i.getName()
-    #             des = i.getDescription()
-    #             tup = {"name": name, "description": des}
-    #             course_data.append(tup)
-    #             req_list = i.getRequirement()  # เรียกเมธอด getRequirement() เพื่อนำข้อมูล Requirement
+    print("Courseforreq")
+    print(course_id)
+    print("Courseforreq")
+    print(course_id)
+    course_data = []
+    req_data = []
+    nonreq_data = []
+    req = ""
+    course = CourseModel()
+    tup2 = {}
+    course.getDataFromDB(course_id)
+    course.getName()
+    course.getDescription()
+    course.getRequirement()
+    tup = {"description": course.getDescription(), "name": course.getName()}
+    if course.getRequirement() != "" or course.getRequirement() != None:
+        tup2 = {"req1": course.getRequirement()}
+        req_data.append(tup2)
+    course_data.append(tup)
+    
 
-    #             for j in req_list:
-    #                 if isinstance(j, Requirement): 
-    #                     if j.getType() == 0:  # ตรวจสอบว่า required_to_fill == 0
-    #                         qest_0 = j.getReq()
-    #                         tup0 = {"qest0": qest_0}
-    #                         nonreq_data.append(tup0)
-    #                     elif j.getType() == 1:  # ตรวจสอบว่า required_to_fill == 1
-    #                         qest_1 = j.getReq()
-    #                         tup1 = {"qest": qest_1}
-    #                         req_data.append(tup1)
-
-
-    return templates.TemplateResponse("course.html", {"request": request})
+    return templates.TemplateResponse("course.html", {"request": request,"courses": course_data, "req": req_data})
